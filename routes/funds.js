d3bb41828788030e768e2d870f69ff25908ebeda
Manny Castaneda <ma52castaneda@gmail.com>
2019-02-27 20:35:09 +0000

    users can now change their bank's name

--- routes/funds.js
+++ routes/funds.js
@@ -20,7 +20,19 @@ router.put("/deposit", function(req,res){
         if(err){
             res.redirect("/funds")
         }else{
-            console.log(req.user)
+            res.redirect("/funds")
+            
+        }
+    })
+    
+})
+
+router.put("/bankname", function(req,res){
+    req.user.bank.name = req.body.bankname
+    User.findByIdAndUpdate(req.user._id, req.user, function(err, updatedUser){
+        if(err){
+            res.redirect("/funds")
+        }else{
             res.redirect("/funds")
             
         }
